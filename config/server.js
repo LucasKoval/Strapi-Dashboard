a4eeea56523872e5a637d0421c9574414816743f
Lucas Koval <lucaskovaldev@gmail.com>
2021-10-29 14:12:29 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '56e0bb96b674220d8c71b908a0333ce2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1da40a67-f55e-40fc-bf15-47d39fc1d33e'
+};
